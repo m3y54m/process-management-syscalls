dd0c1b45f1e30d3eea0cc012b08d189b716e8ae1
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-11-22 20:22:24 +0330

    Make the code readable and add comments

--- src/fork.c
+++ src/fork.c
@@ -6,22 +6,32 @@
 
 int main(void)
 {
+    // Get the process ID of the current process
+    pid_t main_pid = getpid();
+
+    printf("Main Process ID: %d\n", main_pid);
+
+    // Fork a child process
     pid_t pid = fork();
 
     if (pid < 0)
     {
         perror("Fork failed!\n");
+        // Exit with an error code
         exit(1);
     }
     else if (pid == 0)
     {
-        printf("Hello from child!\n");
+        printf("\n[ THIS IS CHILD PROCESS ]\n");
+
         char *argv[] = {"./test", "a", "b", "c", NULL};
+        // Execute the program
         execv("./test", argv);
     }
     else
     {
-        printf("Hello from parent!\n");
+        printf("\n[ THIS IS PARENT PROCESS ]\nForked Process ID: %d\n", pid);
+        // Wait for child process to finish
         wait(NULL);
     }
 
--- src/test.c
+++ src/test.c
@@ -1,15 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>     // for printf()
+#include <sys/types.h> // for pid_t
+#include <unistd.h>    // for getpid(), getppid()
 
 int main(int argc, char *argv[])
 {
     int index;
+    // Get the process ID of the current process
+    pid_t pid = getpid();
+    // Get the process ID of the parent process
+    pid_t ppid = getppid();
 
-    printf("\n[ JUST FOR TEST ]\n");
+    printf("\n\t[ START: TEST PROGRAM ]\n");
+    printf("\tPID: %d\n", pid);
+    printf("\tPPID: %d\n", ppid);
+    printf("\targc = %d\n", argc);
 
-    printf("argc = %d\n", argc);
-
+    // Print the command line arguments
     for (index = 0; index < argc; index++)
-        printf("argv[%d] = %s\n", index, argv[index]);
+        printf("\targv[%d] = %s\n", index, argv[index]);
 
+    printf("\t[ END: TEST PROGRAM ]\n");
     return 0;
 }
